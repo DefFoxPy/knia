627c962f883be6e7312dbcf9acb3a406234dc9e7
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-30 18:22:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-APPLICATION_ID = os.getenv('APPLICATION_ID')
-ID_SERVER = os.getenv('SERVER_ID') 
+#load_dotenv()
+TOKEN = os.environ['DISCORD_TOKEN']
+#APPLICATION_ID = os.environ['APPLICATION_ID']
+#ID_SERVER = os.environ['SERVER_ID'] 
