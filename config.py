3a1e51fcfe46f0c07eddd7869eece28df7877a01
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-09-16 11:17:24 -0400

    chore: config.py
    poder usar al bot de manera local

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv #debug
 
-#load_dotenv()
+load_dotenv() #debug
 TOKEN = os.environ['DISCORD_TOKEN']
 APPLICATION_ID = os.environ['APPLICATION_ID']
 #ID_SERVER = os.environ['SERVER_ID'] 
