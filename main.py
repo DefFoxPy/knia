51b2d3b8c47631d97b6d330a6cb0d02b7c3249de
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-09-16 11:16:58 -0400

    refactor main.py
    agregando una actividad y configurando el prefix

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('z!'), intents=intents, application_id=config.APPLICATION_ID)
+prefix = 'z!'
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), intents=intents, application_id=config.APPLICATION_ID)
 
 @bot.event
 async def on_ready():
+    activity = discord.Game(name='Bot bug dance', type=3)
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
     print('logged on as', bot.user)
 
 @bot.event
 async def on_message(message):
-    # Primero procese los eventos
-    await bot.process_commands(message) 
 
-    # don't respond to command_prefix
-    if message[0:2] == 'z!':
-        return
+    # Primero procese los eventos
+    if message.content.startswith(prefix):
+        await bot.process_commands(message) 
 
     # don't respond to ourselves
-    if message.author == bot.user:
+    elif message.author == bot.user:
         return
     
-    if message.content == 'ping':
+    elif message.content == 'ping':
         await message.channel.send('Pong.')
 
 async def load():
